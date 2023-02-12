ed21bd8ce38f75dd66714b76281afdd83a076907
Keijiro Takahashi <keijiro@gmail.com>
2023-02-12 11:55:36 +0900

    Use TestTools

--- Assets/Script/Visualizer.cs
+++ Assets/Script/Visualizer.cs
@@ -1,19 +1,15 @@
 using UnityEngine;
-using UI = UnityEngine.UI;
-using System.Linq;
-
-namespace MediaPipe.Iris {
+using UnityEngine.UI;
+using Klak.TestTools;
+using MediaPipe.Iris;
 
 public sealed class Visualizer : MonoBehaviour
 {
     #region Editable attributes
 
-    [SerializeField] Texture2D _image = null;
-    [SerializeField] WebcamInput _webcam = null;
-    [Space]
-    [SerializeField] UI.RawImage _previewUI = null;
-    [Space]
+    [SerializeField] ImageSource _source = null;
     [SerializeField] ResourceSet _resources = null;
+    [SerializeField] RawImage _previewUI = null;
     [SerializeField] Shader _shader = null;
     [SerializeField] RectTransform _markerPrefab = null;
 
@@ -22,24 +18,8 @@ public sealed class Visualizer : MonoBehaviour
     #region Private members
 
     EyeLandmarkDetector _detector;
+    RectTransform[] _markers = new RectTransform[5];
     Material _material;
-    RectTransform[] _markers;
-
-    void RunDetector(Texture input)
-    {
-        _detector.ProcessImage(input);
-
-        _material.SetBuffer("_Vertices", _detector.VertexBuffer);
-
-        var scale = ((RectTransform)_previewUI.transform).rect.size;
-        _markers[0].anchoredPosition = _detector.GetIrisCenter() * scale;
-        _markers[1].anchoredPosition = _detector.GetEyelidLeft() * scale;
-        _markers[2].anchoredPosition = _detector.GetEyelidRight() * scale;
-        _markers[3].anchoredPosition = _detector.GetEyelidUpper() * scale;
-        _markers[4].anchoredPosition = _detector.GetEyelidLower() * scale;
-
-        _previewUI.texture = input;
-    }
 
     #endregion
 
@@ -47,13 +27,15 @@ public sealed class Visualizer : MonoBehaviour
 
     void Start()
     {
+        // Eye landmark detector initialization
         _detector = new EyeLandmarkDetector(_resources);
-        _material = new Material(_shader);
-        _markers = Enumerable.Range(0, 5)
-          .Select(x => Instantiate(_markerPrefab, _previewUI.transform))
-          .ToArray();
 
-        if (_image != null) RunDetector(_image);
+        // Marker population
+        for (var i = 0; i < _markers.Length; i++)
+            _markers[i] = Instantiate(_markerPrefab, _previewUI.transform);
+
+        // Shader initialization
+        _material = new Material(_shader);
     }
 
     void OnDestroy()
@@ -64,13 +46,25 @@ public sealed class Visualizer : MonoBehaviour
 
     void LateUpdate()
     {
-        if (_webcam != null) RunDetector(_webcam.Texture);
+        // Eye landmark detection
+        _detector.ProcessImage(_source.Texture);
 
+        // Marker update
+        var scale = ((RectTransform)_previewUI.transform).rect.size;
+        _markers[0].anchoredPosition = _detector.GetIrisCenter() * scale;
+        _markers[1].anchoredPosition = _detector.GetEyelidLeft() * scale;
+        _markers[2].anchoredPosition = _detector.GetEyelidRight() * scale;
+        _markers[3].anchoredPosition = _detector.GetEyelidUpper() * scale;
+        _markers[4].anchoredPosition = _detector.GetEyelidLower() * scale;
+
+        // Contour line draw
         var bounds = new Bounds(Vector3.zero, Vector3.one);
+        _material.SetBuffer("_Vertices", _detector.VertexBuffer);
         Graphics.DrawProcedural(_material, bounds, MeshTopology.Lines, 64, 1);
+
+        // UI update
+        _previewUI.texture = _source.Texture;
     }
 
     #endregion
 }
-
-} // namespace MediaPipe
